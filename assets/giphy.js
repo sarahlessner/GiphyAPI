d26c1a9ffd3389bb75993b55e6bd4b46c1527f0e
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-05-28 13:59:30 -0700

    rating and still img displaying on button click

--- assets/giphy.js
+++ assets/giphy.js
@@ -25,22 +25,51 @@ function renderButtons() {
           button.text(topics[i]);
           // Adding the button to the buttons-view div
           $("#puppybuttons").append(button);
-        }
+        };
 };
 
 renderButtons();
-//on click function for all buttons that will:
 
-	//empty puppies div (where gifs display)
+//function for all buttons that will display gifs and relevant info to HTML
+function displayPuppies(){
 
-	//use AJAX to access Giphy API (key dc6zaTOxFJmzC) and:
+	//use AJAX to access specific breed button being clicked
+	var breed = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + breed + "+puppy&limit=10&sort=relevant&api_key=dc6zaTOxFJmzC";
 
-		//pull 10 gifs related to the topic of the button that was pushed
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+    	//div to hold the breed being pulled
+    	var breedDiv = $("<div class='breed'>");
+    	
+		//display gif rating for each of the 10 gifs being pulled for the breed
+		for (var j = 0; j < response.data.length; j++) {
 
-		//display gif rating
+			var rating = response.data[j].rating;
+			var displayRating = $("<p>").text("Rating: " + rating);
+			breedDiv.append(displayRating);
 
-		//display still image of gif
+			var still = response.data[j].images.fixed_height_still.url;
+			var displayStill = $("<img>").attr("src", still);
+			breedDiv.append(displayStill);
 
-	
+			$("#puppies").prepend(breedDiv);
+		};
+		
+		
+		
+	});
+
+};	
+
+	//onclick for displayPuppies
+	$(document).on("click", ".breed", displayPuppies);
+
+    // Calling the renderButtons function to display the intial buttons
+ 	renderButtons();
+
+
 //on click to make gif animation start/stop
 
