33ea775edb98ece2eeb86d1fb7a8d96610a4d3dc
Sarah Lessner <sarahlessner@gmail.com>
2017-11-23 13:56:16 -0800

    code cleanup for old project

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,36 +1,28 @@
-//create an array to store my dog breeds
-var topics = ["labrador", "golden retriever", "german shepherd", "shiba inu", "samoyed", "rottweiler", 
+//array to store my dog breeds
+var topics = ["labrador", "golden retriever", "german shepherd", "shiba inu", "samoyed", "rottweiler",
 			"goldendoodle", "bernese mountain dog", "border collie", "husky", "newfoundland"];
-
-//create variable and store result of users "puppy-input" (from HTML form)
-
-//push puppy-input to topics array
 
 //loop array and append a button for each to div id="puppybuttons"
 function renderButtons() {
-
-        //empty puppy buttons display area
-        $("#puppybuttons").empty();
-
-        // Loop through the array of breeds
-        for (var i = 0; i < topics.length; i++) {
-
-        //generate button for each breed
-          var button = $("<button>");
-          // Adding a class of breed to button
-          button.addClass("breed");
-          // Adding a data-attribute
-          button.attr("data-name", topics[i]);
-          // Providing the initial button text
-          button.text(topics[i]);
-          // Adding the button to the buttons-view div
-          $("#puppybuttons").append(button);
-        };
+  //empty puppy buttons display area
+  $("#puppybuttons").empty();
+  // Loop through the array of breeds
+  for (var i = 0; i < topics.length; i++) {
+  //generate button for each breed
+    var button = $("<button>");
+    // Adding a class of breed to button
+    button.addClass("breed");
+    // Adding a data-attribute
+    button.attr("data-name", topics[i]);
+    // Providing the initial button text
+    button.text(topics[i]);
+    // Adding the button to the buttons-view div
+    $("#puppybuttons").append(button);
+  };
 };
 
 //function for all buttons that will display gifs and relevant info to HTML
 function displayPuppies(){
-
 	//use AJAX to access specific breed button being clicked
 	var breed = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + breed + "&limit=10&sort=relevant&api_key=dc6zaTOxFJmzC";
@@ -41,7 +33,7 @@ function displayPuppies(){
     }).done(function(response) {
     	//div to hold the breed being pulled
     	var breedDiv = $("<div class='breedImages'>");
-    	
+
 		//display gif rating for each of the 10 gifs being pulled for the breed
 		for (var j = 0; j < response.data.length; j++) {
 
@@ -60,7 +52,7 @@ function displayPuppies(){
 
 			$("#puppies").prepend(breedDiv);
 
-			//onclick function = 
+			//onclick function =
 			displayImg.on("click", function(){
 				var currentSource = $(this).attr("src");
 				var stillUrl = $(this).attr("data-still");
@@ -69,26 +61,20 @@ function displayPuppies(){
 				if (currentSource === stillUrl)
 					$(this).attr("src", gifUrl);   // change the src to data-gif
 				else
-					$(this).attr("src", stillUrl); // change the src to data-still	
+					$(this).attr("src", stillUrl); // change the src to data-still
 
-			});	
+			});
 		};
 	});
-};	
+};
 
 	$("#add-puppy").on("click", function(event) {
-        
-        event.preventDefault();
-       
-        var puppy = $("#puppy-input").val();
-        topics.push(puppy);
-
-        renderButtons();
+    event.preventDefault();
+    var puppy = $("#puppy-input").val();
+    topics.push(puppy);
+    renderButtons();
 	});
 
 	//onclick for displayPuppies
 	$(document).on("click", ".breed", displayPuppies);
-
  	renderButtons();
-
-
